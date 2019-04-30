8757be1e4f4baa91b8dc72ec467be88c524ff7c4
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-04-29 21:57:49 -0400

    Add files via upload

---
+++ final/README.txt
@@ -0,0 +1,14 @@
+README
+
+
+1. How Big Is A Cache Block?
+	The cache block size is found by probing access times to memory based on powers of two. The time to perform each access is recorded into an array.  These access times are differenced based on each power of two used to access memory and locations at which the time increases by a power of two are recorded and averaged together to obtain the cache block size.
+2. How Big Is The Cache?
+	Similar to the cache block, the cache size is obtained through the same process, except on a much larger scale.  Access are based on powers of two and then the time differences between each power of two is recorded.  Here the significant increas is a  larger power of two than the cache block.
+
+3. How Long Does A Reference To Main Memory Take To Complete?
+	Once we know the power of the cache size we can then find how long it takes to access main memory. We do this by running an array larger than the size of the cache and assigning a dummy value at each iteration. Once we know we have gone far enough to reach main memory we clock the end time.
+
+4. How Long Does A Reference Satisfied From Cache Take To Complete?
+	Similar to how we find the time for main memory to compplete we clock the time before we iterate through an array and clock the time at the end. The difference here is that we stick to random 
+values that we know are smaller than the size of the cache.
---
+++ final/cache.h
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+
+
+void cacheSizeCalculation(int powOf2, int* switchSpot,int* count, int timeVariation,int stepModifer);
+		/*cacheSize is a function used to expirementally obtain an estimate of the size of the cache.  
+	Parameters:
+	powOf2: number of powers of 2 to test; wil varry depending on the program
+	switchSpot: pointer to a sum of locations in which their is a significant switch in times
+	count: pointer to a count of the number of significant switches
+	timeVariation: factor by which times need to vary to be considered significant 
+	stepModifer: modifies the step based on what values are trying to be accessed.
+
+	*/
+
+
+
+int cacheSize(int attempts);
+/* Cache size calls the cacheSizeCalculation to calculate an average of access.  
+	 It returns the cache size
+	 Paramaters:
+	 attempts: total number of times to run cacheSizeCalculation, minimum recommendation of 500  
+	*/
+
+
+
+
+
+int blockSize(int attempts);
+	/* block size calls the cacheSizeCalculation to calculate an average of access.  
+	 It returns the size of the block
+	 Paramaters:
+	 attempts: total number of times to run cacheSizeCalculation, minimum recommendation of 500 
+	*/
+
+
+double mainMemoryCalc (int cache,int block);
+/* mainMemoryCalc calucaltes the access time to main memory and returns it
+ 	Paramaters:
+	cache: size of cache
+	block: size of block
+
+*/
+
+
+double cacheMemoryCalc (int cache, int block );
+/* this function calculates the access times for the cache memory and returns it
+	parameters:
+	cache: size of the cache
+	block: size of the lock
+
+*/
+
+
+double cacheMemory(int cache, int tryTimes, int block);
+/* cacheMemory computes the average cache access time and returns it
+	Parameters:
+	cache: size of cache
+	tryTimes: number of times to attempt and average;
+	block: size of block
+*/
+
+
+double mainMemory(int cache, int tryTimes, int block);
+/* mainMemory computes the average cache access time and returns it
+	Parameters:
+	cache: size of cache
+	tryTimes: number of times to attempt and average;
+	block: size of block
+*/
+
+int main(int argc, char** argv);
+
